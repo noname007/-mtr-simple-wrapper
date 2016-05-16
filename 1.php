0104743fdfb5d9b49789798e0af4498faf7477fa
yangzz <1479418380@qq.com>
2016-05-16 18:20:24 +0800

    mtr

--- 1.php
+++ 1.php
@@ -3,9 +3,13 @@
         "gs.push.rgbvr.com",
         "dl.push.rgbvr.com"
     ];
+    $res_ret = [];
     foreach($domain as $v){
         echo $v," ";
         $res = system("sudo mtr  -i 0.1 --report -c 10 -n $v|awk -f 1.awk");
+        $res_ret[$v] = $res;
         #var_dump($res);
     }
-
+    var_dump($res_ret);
+    asort($res_ret);
+    echo json_encode($res_ret);
